e9dc24b868a667593f4ae3471ddc03d1a63eb98c
Ahmad Reza <ahmad.reza4816@gmail.com>
2021-10-12 01:15:07 +0700

    Init Commit Master

---
+++ Master/Assets/Script/BackToMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BackToMenu : MonoBehaviour
+{
+
+    private void Update()
+    {
+        //mengembalikan kembali ke Menu Selection
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            SceneManager.LoadScene(0);
+        }
+
+    }
+}
